cb3b6d84b0b5ef12d0ae77e571ab896941db502b
Julien Tscherrig <julien.tscherrig@hefr.ch>
2016-04-13 00:26:41 +0200

    Test

--- src/main/java/com/tscherrig/ws/WSSystemDB.java
+++ src/main/java/com/tscherrig/ws/WSSystemDB.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
@@ -11,19 +12,24 @@ import javax.ws.rs.core.Response;
 @Path("/db")
 public class WSSystemDB {
 	
-	
 	@GET
-	@Path("/")
+	@Path("/{db}/")
 	public Response list() {
 		String content = "";
 		return Response.status(200).entity(content).build();
 	}
 	
 	@GET
-	@Path("/{id}")
-	public Response get(@PathParam("id") int id) {
-		
+	@Path("/{db}/{id}")
+	public Response detail(@PathParam("id") int id) {
 		return Response.status(404).build();
+	}
+	
+	@POST
+	@Path("/{db}/")
+	public Response list() {
+		String content = "";
+		return Response.status(200).entity(content).build();
 	}
 	
 }
